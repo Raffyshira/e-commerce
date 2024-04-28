e13f5b4f5a473531863640fd4adb447b89772770
Raffyshira <158991786+Raffyshira@users.noreply.github.com>
2024-04-28 18:45:28 +0700

    Update main.jsx
    Remove ReportWebVital

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -15,4 +14,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </React.StrictMode>
 );
 
-reportWebVitals()
